601baeec83336dca7b6e8a04c0a79cc1f643bd47
Paulius Velesko <paulius.velesko@intel.com>
2020-08-12 06:25:51 +0000

    MDRange Placeholder

--- core/src/SYCL/Kokkos_SYCL_KernelLaunch.hpp
+++ core/src/SYCL/Kokkos_SYCL_KernelLaunch.hpp
@@ -9,8 +9,12 @@ namespace Kokkos {
 namespace Experimental {
 namespace Impl {
 
+// Range Launch
 template<class Driver>
 void sycl_launch(const Driver driver) {
+//#ifndef __SYCL_DEVICE_ONLY__
+  isTriviallyCopyable<decltype(driver.m_functor)>();
+//#endif
        driver.m_policy.space().impl_internal_space_instance()->m_queue->wait();
        driver.m_policy.space().impl_internal_space_instance()->m_queue->submit([&] (cl::sycl::handler& cgh) {
          cgh.parallel_for (
@@ -21,6 +25,45 @@ void sycl_launch(const Driver driver) {
       });
       driver.m_policy.space().impl_internal_space_instance()->m_queue->wait();
 }
+// MDRange Launch
+template<class FunctorType, class ...Traits>
+void sycl_launch(const Kokkos::Impl::ParallelFor<FunctorType, MDRangePolicy<Traits...>, Kokkos::Experimental::SYCL> driver) {
+       throw "FAAAA";
+//       typedef decltype(driver) Driver;
+//
+//       typedef Kokkos::MDRangePolicy<Traits...> MDRangePolicy;
+//       typedef typename MDRangePolicy::impl_range_policy Policy;
+//       typedef typename MDRangePolicy::work_tag WorkTag;
+//
+//       typedef typename Policy::WorkRange WorkRange;
+//       typedef typename Policy::member_type Member;
+//
+//       const Policy m_policy;  // construct as RangePolicy( 0, num_tiles
+//
+//       driver.m_policy.space().impl_internal_space_instance()->m_queue->wait();
+//       size_t flatRange = driver.m_policy.m_lower.size();
+//       std::cout << "flatRange = " << flatRange << "\n";
+//       exit(0);
+//       #ifndef SYCL_USE_BIND_LAUNCH
+//       driver.m_policy.space().impl_internal_space_instance()->m_queue->submit([&] (cl::sycl::handler& cgh) {
+//         //CGH_PARALLEL_FOR (
+//         #ifdef SYCL_JUST_DONT_NAME_KERNELS
+//         cgh.parallel_for (//<class kokkos_sycl_functor> (
+//         #else
+//         cgh.parallel_for <class kokkos_sycl_functor<Driver>> (
+//         #endif
+//            cl::sycl::range<1>(flatRange), [=] (cl::sycl::item<1> item) {
+//              int id = item.get_linear_id();
+//                driver.m_functor(id);        
+//         });
+//      });
+//      driver.m_policy.space().impl_internal_space_instance()->m_queue->wait();
+//      #else
+//      driver.m_policy.space().impl_internal_space_instance()->m_queue->submit(
+//        std::bind(Kokkos::Experimental::Impl::sycl_launch_bind<Driver>,driver,std::placeholders::_1));
+//      #endif
+}
+
 
 
 } // Impl namespace
