c630e4e8ef23fcdf26e6bae0caa305e0b342626d
Paulius Velesko <paulius.velesko@intel.com>
2020-08-13 16:15:27 +0000

    moved Type Checks into a separate file
    Conflicts:
    	core/src/Kokkos_SYCL.hpp
    	core/src/Kokkos_View.hpp
    
    triviallyCopyable check now print OBJ name

--- core/src/SYCL/Kokkos_TypeChecks.hpp
+++ core/src/SYCL/Kokkos_TypeChecks.hpp
@@ -2,33 +2,26 @@
 #define KOKKOS_TYPECHECKS_HPP
 
 #include <type_traits>
-template <class Obj>
-void isTriviallyCopyable() {
-  static_assert(std::is_trivially_copyable<Obj>::value);
-
-  static_assert(std::is_copy_constructible<Obj>::value ||
-                    std::is_move_constructible<Obj>::value ||
-                    std::is_copy_assignable<Obj>::value ||
-                    std::is_move_assignable<Obj>::value,
-                "Obj copy/move constructors/assignments deleted");
-
-  static_assert(std::is_trivially_copy_constructible<Obj>::value ||
-                    !std::is_copy_constructible<Obj>::value,
-                "Obj not trivially copy constructible");
-
-  static_assert(std::is_trivially_move_constructible<Obj>::value ||
-                    !std::is_move_constructible<Obj>::value,
-                "Obj not trivially move constructible");
-
-  static_assert(std::is_trivially_copy_assignable<Obj>::value ||
-                    !std::is_copy_assignable<Obj>::value,
-                "Obj not trivially copy assignable");
-
-  static_assert(std::is_trivially_move_assignable<Obj>::value ||
-                    !std::is_move_assignable<Obj>::value,
-                "Obj not trivially move assignable");
-
-  static_assert(std::is_trivially_destructible<Obj>::value,
-                "Obj not trivially destructible");
+#define isTriviallyCopyable(...)\
+  static_assert(std::is_trivially_copyable<__VA_ARGS__>::value);\
+  static_assert(std::is_copy_constructible<__VA_ARGS__>::value ||\
+                    std::is_move_constructible<__VA_ARGS__>::value ||\
+                    std::is_copy_assignable<__VA_ARGS__>::value ||\
+                    std::is_move_assignable<__VA_ARGS__>::value,\
+                #__VA_ARGS__ "copy/move constructors/assignments deleted");\
+  static_assert(std::is_trivially_copy_constructible<__VA_ARGS__>::value ||\
+                    !std::is_copy_constructible<__VA_ARGS__>::value,\
+                #__VA_ARGS__ "not trivially copy constructible");\
+  static_assert(std::is_trivially_move_constructible<__VA_ARGS__>::value ||\
+                    !std::is_move_constructible<__VA_ARGS__>::value,\
+                #__VA_ARGS__ "not trivially move constructible");\
+  static_assert(std::is_trivially_copy_assignable<__VA_ARGS__>::value ||\
+                    !std::is_copy_assignable<__VA_ARGS__>::value,\
+                #__VA_ARGS__ "not trivially copy assignable");\
+  static_assert(std::is_trivially_move_assignable<__VA_ARGS__>::value ||\
+                    !std::is_move_assignable<__VA_ARGS__>::value,\
+                #__VA_ARGS__ "not trivially move assignable");\
+  static_assert(std::is_trivially_destructible<__VA_ARGS__>::value,\
+                #__VA_ARGS__ "not trivially destructible");
 }
 #endif
